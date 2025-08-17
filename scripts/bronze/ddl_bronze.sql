8ad44f92936c9c6dc0a689ade850e163b0793138
Hovhannes Galstyan <86681518+hovo21003@users.noreply.github.com>
2025-08-17 15:31:59 +0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -62,9 +62,9 @@ cntry NVARCHAR(50)
 );
 GO
 
-IF OBJECT_ID ('bronze.erb_px_cat_g1v2', 'U') IS NOT NULL
-	DROP TABLE bronze.erb_px_cat_g1v2;
-CREATE TABLE bronze.erb_px_cat_g1v2(
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
 id			NVARCHAR(50),
 cat			NVARCHAR(50),
 subcat		NVARCHAR(50),
